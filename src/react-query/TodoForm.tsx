5bc1dc4d8ee79e20a0b8018b27485faabcae184c
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-11 11:37:04 +0100

    mutating data and handling errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,17 +1,62 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+
+    onSuccess: (savedTodo, newTodo) => {
+      /*
+      APPROACH 1: Invalidating the cache(refetching)
+
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      })
+
+      */
+      // APPROACH 2: Updating the cache directly
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+        }}
+        className="row mb-3"
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </>
   );
 };
 
