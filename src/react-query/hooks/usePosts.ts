9199751057de9f8b7d4b299fd55fcfe2b323764e
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-11 10:56:51 +0100

    implement infinite scroll

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import axios, { all } from "axios";
 
 export interface Post {
   id: number;
@@ -9,29 +9,31 @@ export interface Post {
 }
 interface PostQuery {
   userId?: number;
-  page: number;
   pageSize: number;
 }
 // userId: number | undefined
 const usePosts = (query: PostQuery) =>
-  useQuery<Post[], Error>({
+  useInfiniteQuery<Post[], Error>({
     // just to structure the cache and make a deps. it's like endpoints users/1/posts
     //any time a deps change, react-query will refetch the data
-    queryKey: query.userId
-      ? ["users", query.userId, "posts", query]
-      : ["posts", query],
-    queryFn: () =>
+    queryKey: query.userId ? ["users", query.userId, "posts"] : ["posts"],
+    queryFn: ({ pageParam = 1 }) =>
       axios
         .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
           params: {
             userId: query.userId,
-            _start: (query.page - 1) * query.pageSize,
+            _start: (pageParam - 1) * query.pageSize,
             _limit: query.pageSize,
           },
         })
         .then((res) => res.data),
     staleTime: 10 * 1000,
     keepPreviousData: true,
+    // will be returned automatically to the queryFn
+    getNextPageParam: (lastPage, allPages) => {
+      //1->2
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 
 export default usePosts;
